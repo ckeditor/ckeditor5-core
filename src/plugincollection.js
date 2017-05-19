109e085b2d1a70edc6ada03cc33feae0e9ef3272
Kamil Piechaczek <pomek@pomek.pl>
2017-05-19 10:38:10 +0200

    Code style: Aligned to ESLint.

--- src/plugincollection.js
+++ src/plugincollection.js
@@ -57,7 +57,7 @@ export default class PluginCollection {
 	/**
 	 * Collection iterator. Returns `[ PluginConstructor, pluginInstance ]` pairs.
 	 */
-	*[ Symbol.iterator ]() {
+	* [ Symbol.iterator ]() {
 		for ( const entry of this._plugins ) {
 			if ( typeof entry[ 0 ] == 'function' ) {
 				yield entry;
