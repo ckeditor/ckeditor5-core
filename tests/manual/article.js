3f8dc1053f2ad6dcea173128ef092cd0a51db177
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-11-06 21:29:46 +0100

    Playground.

--- tests/manual/article.js
+++ tests/manual/article.js
@@ -3,15 +3,46 @@
  * For licensing, see LICENSE.md.
  */
 
-/* globals console, window, document */
+/* globals console, window, document, setInterval */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import ArticlePluginSet from '../_utils/articlepluginset';
 
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
+import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
+
+import { downcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
+import { upcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
+
+import { getData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
+
+function MyPlugin( editor ) {
+	editor.model.schema.register( 'div', {
+		allowIn: [ '$root', 'div' ],
+		isObject: true
+	} );
+
+	editor.model.schema.extend( '$text', {
+		allowIn: 'div'
+	} );
+
+	editor.conversion.for( 'downcast' ).add( downcastElementToElement( {
+		model: 'div',
+		view: ( modelElement, writer ) => {
+			return toWidget( writer.createContainerElement( 'div', { class: 'widget' } ), writer );
+		}
+	} ) );
+
+	editor.conversion.for( 'upcast' ).add( upcastElementToElement( {
+		model: 'div',
+		view: 'div'
+	} ) );
+}
+
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ ArticlePluginSet ],
+		plugins: [ ArticlePluginSet, Widget, MyPlugin ],
 		toolbar: [
 			'heading',
 			'|',
@@ -39,6 +70,10 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
+
+		setInterval( () => {
+			console.log( getData( editor.model ) );
+		}, 3000 );
 	} )
 	.catch( err => {
 		console.error( err.stack );
