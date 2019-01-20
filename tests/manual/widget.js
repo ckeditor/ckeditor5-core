c16e2a5736a04acfc8f755f4664a16ff81220d94
Piotrek Koszuliński <pkoszulinski@gmail.com>
2019-01-20 18:34:03 +0100

    Code refactoring.

--- tests/manual/widget.js
+++ tests/manual/widget.js
@@ -12,8 +12,6 @@ import { downcastElementToElement } from '@ckeditor/ckeditor5-engine/src/convers
 import { upcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
 import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 
-const superFieldSymbol = Symbol( 'superField' );
-
 class SuperField extends Plugin {
 	init() {
 		const editor = this.editor;
@@ -30,103 +28,31 @@ class SuperField extends Plugin {
 			allowAttributes: [ 'counter', 'dropdown', 'date' ]
 		} );
 
+		// Add converters.
+
 		conversion.for( 'editingDowncast' ).add( downcastElementToElement( {
 			model: 'superField',
 			view: ( modelElement, viewWriter ) => {
-				const div = createSuperFieldElement( viewWriter, modelElement );
+				// Create the widget view element.
+				const div = toWidget( createSuperFieldElement( viewWriter, modelElement ), viewWriter );
 
-				viewWriter.setCustomProperty( superFieldSymbol, true, div );
+				// Create a wrapper for custom UI (must use UIElement!).
+				const renderFunction = createCustomUIRenderer( editor, modelElement );
+				const customUIWrapper = viewWriter.createUIElement( 'div', {}, renderFunction );
 
-				const uiWrap = viewWriter.createUIElement( 'div', {}, function( domDocument ) {
-					const domUiWrap = this.toDomElement( domDocument );
+				// And insert it inside the widget element.
+				viewWriter.insert( viewWriter.createPositionAt( div, 0 ), customUIWrapper );
 
-					// Add a custom button to the widget.
-					const domButton = domDocument.createElement( 'button' );
-
-					domButton.addEventListener( 'click', () => {
-						model.change( writer => {
-							const attribute = modelElement.getAttribute( 'counter' );
-
-							const oldValue = parseInt( attribute );
-							const newValue = oldValue + 1;
-
-							console.log( `Change button: ${ oldValue } -> ${ newValue }` );
-
-							writer.setAttribute( 'counter', newValue, modelElement );
-							domButton.innerText = `Increment (${ modelElement.getAttribute( 'counter' ) })`;
-						} );
-					} );
-					domButton.innerText = `Increment (${ modelElement.getAttribute( 'counter' ) })`;
-					domButton.setAttribute( 'disabled', 'disabled' );
-
-					domUiWrap.appendChild( domButton );
-
-					// Add a custom input to the widget.
-					const domInput = domDocument.createElement( 'input' );
-					domInput.setAttribute( 'value', modelElement.getAttribute( 'date' ) );
-					domInput.addEventListener( 'input', evt => {
-						model.change( writer => {
-							const oldValue = modelElement.getAttribute( 'input' );
-							const newValue = evt.srcElement.value;
-
-							console.log( `Change input: ${ oldValue } -> ${ newValue }` );
-
-							writer.setAttribute( 'date', newValue, modelElement );
-						} );
-					} );
-
-					domInput.className = 'jq-datepicker';
-					domInput.setAttribute( 'disabled', 'disabled' );
-
-					domUiWrap.appendChild( domInput );
-
-					// Add a custom select to the widget.
-					const domSelect = domDocument.createElement( 'select' );
-					domSelect.setAttribute( 'disabled', 'disabled' );
-
-					domSelect.addEventListener( 'change', evt => {
-						model.change( writer => {
-							const oldValue = modelElement.getAttribute( 'dropdown' );
-							const newValue = evt.srcElement.value;
-
-							console.log( `Change dropdown: ${ oldValue } -> ${ newValue }` );
-
-							writer.setAttribute( 'dropdown', newValue, modelElement );
-						} );
-					} );
-
-					loadDropdownOptions()
-						.then( data => {
-							for ( const { value, label } of data ) {
-								const domOption = domDocument.createElement( 'option' );
-								domOption.setAttribute( 'value', value );
-								domOption.innerText = label;
-
-								domSelect.appendChild( domOption );
-							}
-						} )
-						.then( () => {
-							window.$( '.jq-datepicker' ).datepicker();
-
-							// Enable the form:
-							domInput.removeAttribute( 'disabled' );
-							domButton.removeAttribute( 'disabled' );
-							domSelect.removeAttribute( 'disabled' );
-						} );
-
-					domUiWrap.appendChild( domSelect );
-
-					// Below code is used to prevent CKEditor from handling events on elements inside a widget.
-					preventCKEditorHandling( domButton, editor );
-					preventCKEditorHandling( domSelect, editor );
-					preventCKEditorHandling( domInput, editor );
-
-					return domUiWrap;
-				} );
-
-				viewWriter.insert( viewWriter.createPositionAt( div, 0 ), uiWrap );
-
-				return toWidget( div, viewWriter );
+				// Returns the following structure:
+				//
+				// 	<div>				   // widget element
+				//		<div>			   // UIElement (wrapper for the custom UI)
+				//			<button />
+				//			<input />      // will be turned into a jQuery date picker
+				//			<dropdown />
+				//		</div>
+				// </div>
+				return div;
 			}
 		} ) );
 
@@ -151,6 +77,109 @@ class SuperField extends Plugin {
 			}
 		} ) );
 	}
+}
+
+function createCustomUIRenderer( editor, modelElement ) {
+	return function renderCustomUI( domDocument ) {
+		const domUiWrap = this.toDomElement( domDocument );
+
+		// ==================================
+		// Add a custom button to the widget.
+		const domButton = domDocument.createElement( 'button' );
+
+		domButton.addEventListener( 'click', () => {
+			editor.model.change( writer => {
+				const attribute = modelElement.getAttribute( 'counter' );
+
+				const oldValue = parseInt( attribute );
+				const newValue = oldValue + 1;
+
+				console.log( `Change button: ${ oldValue } -> ${ newValue }` );
+
+				writer.setAttribute( 'counter', newValue, modelElement );
+				domButton.innerText = `Increment (${ modelElement.getAttribute( 'counter' ) })`;
+			} );
+		} );
+		domButton.innerText = `Increment (${ modelElement.getAttribute( 'counter' ) })`;
+		domButton.setAttribute( 'disabled', 'disabled' );
+
+		domUiWrap.appendChild( domButton );
+
+		// ================================
+		// Add a custom input to the widget.
+		// Initialize jQuery date picker on it.
+		// Note: there's a bug that changing the date via the jQ panel will not change it
+		// in the input – that's most likely jQ date picker's bug. It works when changing by typing
+		// in this widget.
+		const domInput = domDocument.createElement( 'input' );
+
+		domInput.setAttribute( 'value', modelElement.getAttribute( 'date' ) );
+		domInput.addEventListener( 'input', evt => {
+			editor.model.change( writer => {
+				const oldValue = modelElement.getAttribute( 'input' );
+				const newValue = evt.srcElement.value;
+
+				console.log( `Change input: ${ oldValue } -> ${ newValue }` );
+
+				writer.setAttribute( 'date', newValue, modelElement );
+			} );
+		} );
+		domInput.className = 'jq-datepicker';
+		domInput.setAttribute( 'disabled', 'disabled' );
+
+		domUiWrap.appendChild( domInput );
+
+		// ==================================
+		// Add a custom select to the widget.
+		// The options available in this dropdown are loaded from an asynchronous (e.g. external)
+		// provider – `loadDropdownOptions()`.
+		const domSelect = domDocument.createElement( 'select' );
+		domSelect.setAttribute( 'disabled', 'disabled' );
+
+		domSelect.addEventListener( 'change', evt => {
+			editor.model.change( writer => {
+				const oldValue = modelElement.getAttribute( 'dropdown' );
+				const newValue = evt.srcElement.value;
+
+				console.log( `Change dropdown: ${ oldValue } -> ${ newValue }` );
+
+				writer.setAttribute( 'dropdown', newValue, modelElement );
+			} );
+		} );
+
+		// =================================
+		// Load the options into the dropdown, enable the jQ date picker and
+		// enable all the form fields once everything is ready.
+
+		loadDropdownOptions()
+			.then( data => {
+				for ( const { value, label } of data ) {
+					const domOption = domDocument.createElement( 'option' );
+					domOption.setAttribute( 'value', value );
+					domOption.innerText = label;
+
+					domSelect.appendChild( domOption );
+				}
+			} )
+			.then( () => {
+				window.$( '.jq-datepicker' ).datepicker();
+
+				// Enable the form:
+				domInput.removeAttribute( 'disabled' );
+				domButton.removeAttribute( 'disabled' );
+				domSelect.removeAttribute( 'disabled' );
+			} );
+
+		domUiWrap.appendChild( domSelect );
+
+		// =======================================
+		// Below code is used to prevent CKEditor from handling events on elements inside a widget.
+		preventCKEditorHandling( domButton, editor );
+		preventCKEditorHandling( domSelect, editor );
+		preventCKEditorHandling( domInput, editor );
+
+		return domUiWrap;
+	};
 }
 
 function loadDropdownOptions() {
@@ -209,6 +238,7 @@ ClassicEditor
 		console.error( err.stack );
 	} );
 
+// Hack needed to make the input fields accessible and responsive when used in a widget.
 function preventCKEditorHandling( domElement, editor ) {
 	// Prevent the editor from listening on below events in order to stop rendering selection.
 	domElement.addEventListener( 'click', stopEventPropagationAndHackRendererFocus, { capture: true } );
