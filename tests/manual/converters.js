ffad76375eecbf5b7a8fa7d1c6972ddcaad7fd2d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2019-02-13 20:59:08 +0100

    Various improvements and clarifications.

--- tests/manual/converters.js
+++ tests/manual/converters.js
@@ -16,9 +16,10 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Font from '@ckeditor/ckeditor5-font/src/font';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import List from '@ckeditor/ckeditor5-list/src/list';
 
 import { upcastElementToAttribute, upcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
 import { downcastAttributeToElement, downcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
@@ -29,43 +30,54 @@ class SpecialConverters extends Plugin {
 		const schema = this.editor.model.schema;
 		const conversion = this.editor.conversion;
 
-		this._handleDivs( schema, conversion );
+		// this._handleDivsOptionA( schema, conversion );
+		this._handleDivsOptionB( schema, conversion );
 
-		this._handleFontFamily( conversion );
+		this._handleAllFontFamilyValues( conversion );
 
-		this._handleFontSize( conversion );
+		this._handleAllFontSizeValues( conversion );
 	}
 
-	_handleDivs( schema, conversion ) {
-		// Option A: Semantic elements
-
+	// Option A: Handle div with a specific attributes.
+	//
+	// This specific div:								<div class="my-special-section" data-poc="false">
+	// Will be converted to a model element called:		<specialSection>
+	//
+	// It's a good option when the number of different types of HTML markup is limited and
+	// you can assign special meanings to those structures. Then, converting them to a more
+	// semantical model elements gives you a great control over how they behave and what markup
+	// is produced by the editor.getData() method.
+	_handleDivsOptionA( schema, conversion ) {
 		// Define special-section block in the schema.
-		// schema.register( 'special-section', {
-		// 	allowWhere: '$block',
-		// 	allowContentOf: [ '$root', '$block' ]
-		// } );
-		//
-		// // Add two-way (view-to-model and model-to-view) converter for special-section.
-		// conversion.elementToElement( {
-		// 	model: 'special-section',
-		// 	view: {
-		// 		name: 'div',
-		// 		classes: 'my-special-section',
-		// 		// If the "data-poc" flag is not needed the whole `attributes` block can be dropped.
-		// 		// After removing it the model-to-view converter will render <div class="my-special-section"></div>
-		// 		attributes: {
-		// 			data-poc: 'false'
-		// 		}
-		// 	}
-		// } );
+		schema.register( 'specialSection', {
+			allowWhere: '$block',
+			allowContentOf: [ '$root', '$block' ]
+		} );
 
-		// Option B: Catch-all attributes div
+		// Add two-way (view-to-model and model-to-view) converter for special-section.
+		conversion.elementToElement( {
+			model: 'specialSection',
+			view: {
+				name: 'div',
+				classes: 'my-special-section',
+				// If the "data-poc" flag is not needed the whole `attributes` block can be dropped.
+				// After removing it the model-to-view converter will render <div class="my-special-section"></div>
+				attributes: {
+					'data-poc': 'false'
+				}
+			}
+		} );
+	}
 
+	// Option B: Handle any <div> with any set of attributes
+	//
+	// This is "catch-all" div converter. It will convert all attributes of that div too.
+	_handleDivsOptionB( schema, conversion ) {
 		schema.register( 'div', {
 			allowWhere: '$block',
 			allowContentOf: [ '$root' ],
-			// The `allAttributes` attribute will hold every attribute from HTML.
-			allowAttributes: [ 'allAttributes' ]
+			// The `attributesMap` attribute will hold every attribute from HTML.
+			allowAttributes: [ 'attributesMap' ]
 		} );
 
 		// View-to-model converter signifies all attributes into one model's attribute:
@@ -74,20 +86,20 @@ class SpecialConverters extends Plugin {
 			model: ( viewElement, modelWriter ) => {
 				const attributes = JSON.stringify( [ ...viewElement.getAttributes() ] );
 
-				return modelWriter.createElement( 'div', { allAttributes: attributes } );
+				return modelWriter.createElement( 'div', { attributesMap: attributes } );
 			}
 		} ) );
 
-		// Downcast converter for element - will convert model div with attribute `allAttributes` to proper view element
+		// Downcast converter for element - will convert model div with attribute `attributesMap` to proper view element
 		// with all attributes.
 		conversion.for( 'downcast' ).add( downcastElementToElement( {
 			model: 'div',
 			view: 'div'
 		} ) );
 
-		// Model-to-view converter that handles changes of the attribute - it will update
+		// Model-to-view converter that handles changes of the attribute maps
 		conversion.for( 'downcast' ).add( dispatcher => {
-			dispatcher.on( 'attribute:allAttributes', ( evt, data, conversionApi ) => {
+			dispatcher.on( 'attribute:attributesMap', ( evt, data, conversionApi ) => {
 				const viewWriter = conversionApi.writer;
 
 				const viewDiv = conversionApi.mapper.toViewElement( data.item );
@@ -109,7 +121,17 @@ class SpecialConverters extends Plugin {
 		} );
 	}
 
-	_handleFontFamily( conversion ) {
+	// Add converters for font family which will catch spans with all possible font-family styles.
+	//
+	// By default the font family features accept only fony-family styles matching the exact options
+	// specified in config.fontFamily.options. The converters defined in this method override the default behaviour.
+	//
+	// However, we still use the values specified in config.fontFamily.options to display items in the "Font family" dropdown.
+	// Threfore, if we have at least a partial match of the font-family style defined in the config, then we want to
+	// select that specific item in the dropdown when the caret is inside such style. That's why the below converter
+	// does a bit of magic with font names normalization and magic. If we didn't care about selecting the right options
+	// in the dropdown, it'd become much simpler.
+	_handleAllFontFamilyValues( conversion ) {
 		const fontFamilyOptions = normalizeOptions( this.editor.config.get( 'fontFamily.options' ) );
 
 		// Add special catch-all converter for font-family feature.
@@ -148,6 +170,7 @@ class SpecialConverters extends Plugin {
 					return fontOption ? fontOption.model : fontFamilyCSS;
 				}
 			},
+
 			// Override default font-family converter:
 			converterPriority: 'high'
 		} ) );
@@ -166,12 +189,14 @@ class SpecialConverters extends Plugin {
 				}
 			},
 			model: 'fontFamily',
+
 			// Override default font-family converter:
 			converterPriority: 'high'
 		} ) );
 	}
 
-	_handleFontSize( conversion ) {
+	// Similar to _handleAllFontFamilyValues() but for font-size.
+	_handleAllFontSizeValues( conversion ) {
 		// Add special catch-all converter for font-size feature.
 		conversion.for( 'upcast' ).add( upcastElementToAttribute( {
 			view: {
@@ -190,7 +215,7 @@ class SpecialConverters extends Plugin {
 					// 12, 14, 'default', 18, 20, 22, 24, 26, 28, 30
 					// Other sizes will be converted to the model but the UI might not be aware of them.
 
-					// The font-size feature expects numeric values to be Number not String.1
+					// The font-size feature expects numeric values to be Number not String.
 					return parseInt( value );
 				}
 			},
@@ -225,8 +250,7 @@ function normalizeFontName( fontName ) {
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		// Add required plugins.
-		plugins: [ Essentials, Heading, Bold, Italic, Strikethrough, Font, SpecialConverters ],
+		plugins: [ Essentials, Heading, Bold, Italic, Strikethrough, Font, List, SpecialConverters ],
 		toolbar: [
 			'heading',
 			'|',
@@ -241,16 +265,13 @@ ClassicEditor
 			'redo'
 		],
 		fontSize: {
-			// If font-sizes are defined providing them is enough for the font-size feature:
 			options: [ 12, 14, 'default', 18, 20, 22, 24, 26, 28, 30 ]
 		},
 		fontFamily: {
 			options: [
 				'default',
 				'Comic Sans MS, cursive',
-				// Below are font in defualt configuration:
 				'Arial, Helvetica, sans-serif',
-				// 'Courier New, Courier, monospace',
 				'Georgia, serif',
 				'Lucida Sans Unicode, Lucida Grande, sans-serif',
 				'Tahoma, Geneva, sans-serif',
