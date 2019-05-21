72ac8dbe43ce019e53b692a870ec8a5d82778150
Maciej Gołaszewski <jodator@jodator.net>
2019-05-21 13:43:28 +0200

    Sample JSON loading and retrieving data.

--- tests/manual/json.js
+++ tests/manual/json.js
@@ -11,8 +11,57 @@ import ArticlePluginSet from '../_utils/articlepluginset';
 import Plugin from '../../src/plugin';
 
 class JSONData extends Plugin {
+	static get pluginName() {
+		return 'JSONData';
+	}
+
 	init() {
 		console.log( 'JSONData.init()' );
+
+		this._initDataPipeline();
+	}
+
+	_initDataPipeline() {
+		const editor = this.editor;
+
+		editor.data.init = function( allRootsData ) {
+			const parsedData = JSON.parse( allRootsData.trim() );
+
+			editor.model.enqueueChange( 'transparent', writer => {
+				const root = editor.model.document.getRoot( parsedData.root );
+
+				createChildren( writer, root, parsedData.children );
+			} );
+		};
+
+		editor.data.stringify = modelElementOrFragment => {
+			let data;
+
+			if ( modelElementOrFragment.is( 'rootElement' ) ) {
+				data = {
+					root: modelElementOrFragment.toJSON(),
+					children: Array.from( modelElementOrFragment.getChildren() ).map( child => child.toJSON() )
+				};
+			} else {
+				data = modelElementOrFragment.toJSON();
+			}
+
+			return JSON.stringify( data );
+		};
+	}
+}
+
+function createChildren( writer, parent, children = [] ) {
+	for ( const child of children ) {
+		if ( !child.name ) {
+			writer.appendText( child.data, child.attributes, parent );
+		} else {
+			const childElement = writer.createElement( child.name, child.attributes );
+
+			writer.append( childElement, parent );
+
+			createChildren( writer, childElement, child.children );
+		}
 	}
 }
 
