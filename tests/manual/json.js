c84832396249cc6fe450f9f8ba4f9fc7a2dfe189
Maciej Gołaszewski <jodator@jodator.net>
2019-05-21 14:12:17 +0200

    Add demo lifecycle buttons.

--- tests/manual/json.js
+++ tests/manual/json.js
@@ -65,37 +65,86 @@ function createChildren( writer, parent, children = [] ) {
 	}
 }
 
-ClassicEditor
-	.create( document.querySelector( '#editor' ), {
-		plugins: [ ArticlePluginSet, JSONData ],
-		toolbar: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		],
-		image: {
-			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
-	} )
-	.then( editor => {
-		window.editor = editor;
-	} )
-	.catch( err => {
-		console.error( err.stack );
+function createEditor( data, parent ) {
+	return ClassicEditor
+		.create( data, {
+			plugins: [ ArticlePluginSet, JSONData ],
+			toolbar: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'blockQuote',
+				'insertTable',
+				'mediaEmbed',
+				'undo',
+				'redo'
+			],
+			image: {
+				toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
+			},
+			table: {
+				contentToolbar: [
+					'tableColumn',
+					'tableRow',
+					'mergeTableCells'
+				]
+			}
+		} )
+		.then( editor => {
+			window.editor = editor;
+
+			parent.appendChild( editor.ui.element );
+		} )
+		.catch( err => {
+			console.error( err.stack );
+		} );
+}
+
+const updateButton = document.getElementById( 'update' );
+const initButton = document.getElementById( 'init' );
+const destroyButton = document.getElementById( 'destroy' );
+const prettyPrint = document.getElementById( 'pretty-print' );
+
+updateButton.addEventListener( 'click', updateData );
+prettyPrint.addEventListener( 'change', updateData );
+
+initButton.addEventListener( 'click', () => {
+	initButton.disabled = true;
+
+	const data = document.getElementById( 'data' ).value;
+
+	createEditor( data, document.getElementById( 'editor' ) ).then( () => {
+		updateButton.disabled = false;
+		prettyPrint.disabled = false;
+		destroyButton.disabled = false;
 	} );
+} );
+
+destroyButton.addEventListener( 'click', () => {
+	updateButton.disabled = true;
+	prettyPrint.disabled = true;
+	destroyButton.disabled = true;
+
+	window.editor.destroy()
+		.then( () => {
+			window.editor.ui.element.remove();
+			window.editor = null;
+			initButton.disabled = false;
+		} );
+} );
+
+function updateData() {
+	let data = window.editor.getData();
+
+	const isPrettyPrint = prettyPrint.checked;
+
+	if ( isPrettyPrint ) {
+		data = JSON.stringify( JSON.parse( data ), undefined, 4 );
+	}
+
+	document.getElementById( 'data' ).value = data;
+}
